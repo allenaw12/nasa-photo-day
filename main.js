d5918b8b1e4a1e65243944506ab9b54a3dd6c1db
allenaw12 <102706128+allenaw12@users.noreply.github.com>
2023-02-15 18:02:20 -0600

    styling and some error handling

--- main.js
+++ main.js
@@ -4,44 +4,37 @@
 let date = new Date();
 let today = date.getFullYear()+"-"+(date.getMonth()+1)+"-"+ date.getDate()
 
+//fetch today's photo on load
 document.querySelector('body').addEventListener('load', getPhotoByDate())
-// fetch(`https://api.nasa.gov/planetary/apod?date=${today}&api_key=HTx6NaJHwsGSGpIZ8me685Y7LBXNP99XNupNb13g`)
-//     .then(res => res.json())
-//     .then(data => {
-//         document.querySelector('.date').innerText = data.date
-//         if(data.media_type === 'image'){
-//             document.querySelector('iframe').style.display = 'none'
-//             document.querySelector('img').src = data.url
-//         }else{
-//             document.querySelector('iframe').src = data.url
-//             document.querySelector('iframe').style.display = 'block'
-//         }
-//         data.copyright ? document.querySelector('.copyright').innerText = "\u00A9" + data.copyright : null
-//         document.querySelector('.explanation').innerText = data.explanation
-//         console.log(data)})
-//     .catch(err => console.log(`Error: ${err}`))
+//fetch requested date on button/enter key
+document.querySelector('#form').addEventListener('submit', getPhotoByDate)
 
 function getPhotoByDate(e){
     e ? e.preventDefault() : null
     let input = document.querySelector('input').value
-    console.log(input)
+    // console.log(input)
     input ? today = input : null
     fetch(`https://api.nasa.gov/planetary/apod?date=${today}&api_key=HTx6NaJHwsGSGpIZ8me685Y7LBXNP99XNupNb13g`)
         .then(res => res.json())
         .then(data => {
+            document.querySelector('#error').innerText = ''
+            if(data.msg){
+                document.querySelector('#error').innerText = data.msg
+                return
+            }
             document.querySelector('.date').innerText = data.date
             if(data.media_type === 'image'){
-                document.querySelector('iframe').style.display = 'none'
+                document.querySelector('#video').style.display = 'none'
                 document.querySelector('img').src = data.url
             }else{
                 document.querySelector('img').src = ''
                 document.querySelector('iframe').src = data.url
-                document.querySelector('iframe').style.display = 'block'
+                document.querySelector('#video').style.display = 'block'
             }
-            data.copyright ? document.querySelector('.copyright').innerText = "\u00A9" + data.copyright : null
+            document.querySelector('#title').innerText = data.title
+            data.copyright ? document.querySelector('.copyright').innerText = "\u00A9" + data.copyright : document.querySelector('.copyright').innerText = ''
             document.querySelector('.explanation').innerText = data.explanation
-            console.log(data)})
+            console.log(data)
+        })
         .catch(err => console.log(`Error: ${err}`))
 }
-
-document.querySelector('#form').addEventListener('submit', getPhotoByDate)
