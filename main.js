2f37bd8ceccab579c606cdf4a2f9d67ba073cb5c
allenaw12 <102706128+allenaw12@users.noreply.github.com>
2023-02-19 17:33:38 -0600

    commented out lines of code that was replaced with flex styling

--- main.js
+++ main.js
@@ -29,8 +29,8 @@ async function getPhotoByDate(e){
     await fetch(`https://api.nasa.gov/planetary/apod?${input}&thumbs=true&api_key=HTx6NaJHwsGSGpIZ8me685Y7LBXNP99XNupNb13g`)
         .then(res => res.json())
         .then(data => {
-            document.getElementById('explanation').style.marginTop = null
-            document.getElementById('copyright').style.marginTop = null
+            // document.getElementById('explanation').style.marginTop = null
+            // document.getElementById('copyright').style.marginTop = null
             if(Array.isArray(data)){
                 data = data[0]
             }
@@ -40,8 +40,8 @@ async function getPhotoByDate(e){
                 document.querySelector('#error').innerText = data.msg
                 return
             }
-            let date = data.date.slice(5) + '-' + data.date.slice(0,4)
-            document.getElementById('photoDate').innerText = date
+            // let date = data.date.slice(5) + '-' + data.date.slice(0,4)
+            document.getElementById('photoDate').innerText = data.date
             if(data.media_type === 'image'){
                 document.querySelector('#video').style.display = 'none'
                 document.querySelector('img').src = data.url
@@ -55,11 +55,11 @@ async function getPhotoByDate(e){
                 document.querySelector('#video').style.display = 'block'
             }
             document.querySelector('#title').innerText = data.title
+            document.querySelector('#explanation').innerText = data.explanation
             let copyrightEl = document.getElementById('copyright')
             let cc = data.copyright
-            document.querySelector('#explanation').innerText = data.explanation
-            if(cc && (cc.includes('\n') || cc.length > 26)){
-                console.log('longggg copyright', cc)
+            if(cc && cc.includes('\n')){
+                // console.log('longggg copyright', cc)
                 // let newLineCount = 1
                 // for(char of cc){
                 //     if(char == String.fromCharCode(10)){
@@ -68,47 +68,47 @@ async function getPhotoByDate(e){
                 //     }
                 // }
                 // if(newLineCount >= 2)
-                if(cc.indexOf('\n') !== cc.lastIndexOf('\n')){
-                    console.log('newlines greater than or equal to 2')
-                    let noLines = cc.split('\n')
-                    cc = noLines.join(' ')
+                // if(cc.indexOf('\n') !== cc.lastIndexOf('\n')){
+                    // console.log('newlines greater than or equal to 2')
+                    cc = cc.split('\n').join(' ')
                     // let middle = Math.floor(noLines.length/2)
                     // let i0 = noLines.length > 2 ? noLines[middle] += '\n' + noLines[middle+1] : noLines
                     // console.log(i0)
                     // cc = i0 !== noLines ? i0.concat(noLines.slice(2).join(' ')) : i0.join(' ')
-                }
-                if(cc.length > 26){
-                    console.log(cc.length, '> 26')
-                    let spaces = cc.split(' ')
-                    let newcc = []
-                    console.log(spaces)
-                    for(i=0;i<spaces.length;i++){
-                        if(i===0){
-                            newcc.push(spaces[i])
-                        }else if(!newcc[1] && (newcc[0].length + spaces[i].length) <= 26){
-                            console.log('newcc 0 concating',i, newcc[0].length + spaces[i].length)
-                            newcc[0] += ' ' + spaces[i]
-                        }else if((newcc[1] && newcc[1].length + spaces[i].length <= 40)||(newcc[2] && newcc[2].length + spaces[i].length <= 37)){
-                            console.log('newcc 1 or 2 concating',i,newcc[2] ? newcc[1].length + spaces[i].length : newcc[1].length + spaces[i].length)
-                            newcc[2] ? newcc[2] += ' ' + spaces[i] : newcc[1] += ' ' + spaces[i]
-                        }else if(!newcc[1] || !newcc[2]){
-                            console.log('newcc index 1 or 2')
-                            newcc.push('\n' + spaces[i])
-                        }else{
-                            console.log('newcc basic push')
-                            newcc.push(spaces[i])
-                        }
-                    }
-                    console.log(newcc)
-                    cc = newcc.join(' ')
-                }
-                console.log(cc.length)
+                // \}
+                // if(cc.length > 26){
+                //     console.log(cc.length, '> 26')
+                //     let spaces = cc.split(' ')
+                //     let newcc = []
+                //     console.log(spaces)
+                //     for(i=0;i<spaces.length;i++){
+                //         if(i===0){
+                //             newcc.push(spaces[i])
+                //         }else if(!newcc[1] && (newcc[0].length + spaces[i].length) <= 26){
+                //             console.log('newcc 0 concating',i, newcc[0].length + spaces[i].length)
+                //             newcc[0] += ' ' + spaces[i]
+                //         }else if((newcc[1] && newcc[1].length + spaces[i].length <= 40)||(newcc[2] && newcc[2].length + spaces[i].length <= 37)){
+                //             console.log('newcc 1 or 2 concating',i,newcc[2] ? newcc[1].length + spaces[i].length : newcc[1].length + spaces[i].length)
+                //             newcc[2] ? newcc[2] += ' ' + spaces[i] : newcc[1] += ' ' + spaces[i]
+                //         }else if(!newcc[1] || !newcc[2]){
+                //             console.log('newcc index 1 or 2')
+                //             newcc.push('\n' + spaces[i])
+                //         }else{
+                //             console.log('newcc basic push')
+                //             newcc.push(spaces[i])
+                //         }
+                //     }
+                //     console.log(newcc)
+                //     cc = newcc.join(' ')
+                // }
+                // console.log(cc.length)
                 // copyrightEl.style.marginTop = "1.6rem"
-                cc.indexOf('\n') !== cc.lastIndexOf('\n') ?
-                document.getElementById('explanation').style.marginTop = "3.5rem" : 
-                document.getElementById('explanation').style.marginTop = "2.5rem"
+                // cc.indexOf('\n') !== cc.lastIndexOf('\n') ?
+                // document.getElementById('explanation').style.marginTop = "3.5rem" : 
+                // document.getElementById('explanation').style.marginTop = "2.5rem"
             }
             cc ? copyrightEl.innerText = "\u00A9" + cc : copyrightEl.innerText = ''
-        })
+        }
+        )
         .catch(err => console.log(`Error: ${err}`))
 }
